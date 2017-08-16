2a84762cf4842f9dd30b29879d78b9f34f7490a0
luckyboy1102 <chenshuai3333@sina.com>
2017-08-16 15:17:26 +0800

    Remaining

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -73,6 +73,11 @@ public class Practice14FlipboardView extends View {
         int y = centerY - bitmapHeight / 2;
 
         canvas.save();
+        canvas.clipRect(0, 0, getWidth(), centerY);
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        canvas.save();
 
         camera.save();
         camera.rotateX(degree);
